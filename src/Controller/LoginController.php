5b4db9fed91f56f4987cf34c0ff5ba44d0f2e5bf
Saad Haroon <saad.haroon@coeus-solutions.de>
2022-05-26 18:12:09 +0500

    basic-user-login-and-registration-added

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Class LoginController
+ * @package App\Controller
+ *
+ */
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class UserController
+ * @package App\Controller
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @return Response
+     * @Route("/home", name="home")
+     */
+    public function index(): Response
+    {
+        // usually you'll want to make sure the user is authenticated first,
+        // see "Authorization" below
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // returns your User object, or null if the user is not authenticated
+        // use inline documentation to tell your editor your exact User class
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+
+        // Call whatever methods you've added to your User class
+        // For example, if you added a getFirstName() method, you can use that.
+        return $this->render('home.html.twig', [
+            'email' => $user->getEmail(),
+        ]);
+    }
+}
