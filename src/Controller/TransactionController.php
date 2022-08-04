0d64cde18223090a03794e7f1daca9fbddb57cf1
Saad Haroon <saad.haroon@coeus-solutions.de>
2022-08-04 14:22:58 +0500

    add-single-recycle

--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -9,13 +9,17 @@ use App\Entity\Recycler;
 use App\Entity\User;
 use App\Enum\RoleEnum;
 use App\Form\BulkReturnFormType;
+use App\Form\RecycleFormType;
 use App\Form\ReturnFormType;
 use App\Helper\CustomHelper;
 use App\Service\BatteryService;
 use App\Service\ManufacturerService;
 use App\Service\RecyclerService;
 use App\Service\TransactionLogService;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -145,18 +149,18 @@ class TransactionController  extends CRUDController
                     CustomHelper::BATTERY_STATUS_RETURNED
                 );
             $return = new BatteryReturn();
-            $return->setUpdated(new \DateTime('now'));
-            $return->setCreated(new \DateTime('now'));
+            $return->setUpdated(new DateTime('now'));
+            $return->setCreated(new DateTime('now'));
             $return->setAddress($formData['information']['address']);
             $return->setCity($formData['information']['city']);
             $return->setCountry($formData['information']['country']);
-            $return->setReturnDate(new \DateTime('now'));
+            $return->setReturnDate(new DateTime('now'));
             $return->setReturnFrom($user);
             $return->setReturnTo($recycler instanceof Recycler ? $recycler : null);
             $return->setBattery($battery);
             $return->setTransactionLog($transactionLog);
             $battery->setStatus(CustomHelper::BATTERY_STATUS_RETURNED);
-            $battery->setUpdated(new \DateTime('now'));
+            $battery->setUpdated(new DateTime('now'));
             $battery->setCurrentPossessor($user);
 
             $this->entityManager->persist($return);
@@ -289,5 +293,97 @@ class TransactionController  extends CRUDController
         return new JsonResponse([
             'recyclers' => $this->recyclerService->toChoiceArray($recyclers)
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function recycleAction(Request $request): Response
+    {
+        /** @var User $user */
+        $user = $this->security->getUser();
+        $manufacturer = null;
+
+        if (!in_array(RoleEnum::ROLE_SUPER_ADMIN, $user->getRoles(), true) &&
+            !in_array(RoleEnum::ROLE_ADMIN, $user->getRoles(), true) &&
+            in_array(RoleEnum::ROLE_MANUFACTURER, $user->getRoles(), true) ) {
+            $recyclers = $this->recyclerService->toChoiceArray($user->getManufacturer()->getRecyclers(), true);
+            $manufacturer = $user->getManufacturer();
+            $recyclers = array_merge([
+                $manufacturer->getName() => $manufacturer
+            ], $recyclers);
+        } else {
+            $recyclers = $this->recyclerService->getAllRecyclers();
+            $recyclers = $this->recyclerService->toChoiceArray($recyclers, true);
+        }
+
+        $form = $this->createForm(RecycleFormType::class, null, [
+            'recyclers' => $recyclers
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted())
+        {
+            $formData = $form->getData();
+            $serialNumber = $formData['battery'] ?? null;
+            /** @var Recycler|null $recycler */
+            $recycler = $formData['recycler'] ?? null;
+
+            if (empty($serialNumber)) {
+                $this->addFlash('sonata_flash_error', 'Kindly Insert Valid Battery Serial Number!');
+                return new RedirectResponse($this->admin->generateUrl('list'));
+            }
+
+            /** @var Battery|null $battery */
+            $battery = $this->batteryService->fetchBatteryBySerialNumber(
+                $serialNumber,
+                $manufacturer,
+                $user->getManufacturer() ? false : true
+            );
+
+            if (empty($battery) || $battery->getStatus() === CustomHelper::BATTERY_STATUS_PRE_REGISTERED) {
+                $this->addFlash('sonata_flash_error', 'Battery may not exist or registered!');
+                return new RedirectResponse($this->admin->generateUrl('recycle'));
+            }
+
+            if (CustomHelper::BATTERY_STATUSES[$battery->getStatus()] >=
+                CustomHelper::BATTERY_STATUSES[CustomHelper::BATTERY_STATUS_RECYCLED] ||
+                ($this->transactionLogService->isExist($battery, CustomHelper::BATTERY_STATUS_RECYCLED))
+            ) {
+                $this->addFlash('sonata_flash_error', 'Battery is already recycled!');
+
+                return new RedirectResponse($this->admin->generateUrl('list'));
+            }
+
+            /** If Admin / Super Admin - we will use battery's manufacturer's User */
+            if (in_array(RoleEnum::ROLE_SUPER_ADMIN, $user->getRoles(), true)) {
+                $user = $battery->getManufacturer()->getUser();
+            }
+
+            $battery->setStatus(CustomHelper::BATTERY_STATUS_RECYCLED);
+            $battery->setUpdated(new DateTime('now'));
+            $battery->setCurrentPossessor($user);
+            $this->transactionLogService
+                ->createReturnTransactionLog(
+                    $battery,
+                    $user,
+                    $recycler instanceof Recycler ? $recycler : null,
+                    null,
+                    CustomHelper::BATTERY_STATUS_RECYCLED
+                );
+            $this->addFlash('sonata_flash_success', 'Recycle Added Successfully!');
+
+            return new RedirectResponse($this->admin->generateUrl('list'));
+        }
+
+        return $this->render(
+            'recycle/add_single_recycle.html.twig',
+            array(
+                'form' => $form->createView(),
+            )
+        );
     }
 }
